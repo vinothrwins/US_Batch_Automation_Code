61c39f2e14dad191062936b06089c015874fee9d
Vinoth Rathinam <44257903+vinothrwins@users.noreply.github.com>
2020-11-08 19:11:04 +0530

    Updated the code

--- TestNG/src/demo/AlphabeticOrder.java
+++ TestNG/src/demo/AlphabeticOrder.java
@@ -5,9 +5,10 @@ import org.testng.annotations.Test;
 public class AlphabeticOrder {
   	
  // Code is updated by Divya 
+ // Code is updated by VInoth 
   @Test
-  public void za() {
-	  System.out.println("Method ZA is executed");
+  public void z() {
+	  System.out.println("Method Z is executed");
   }
 	  
   @Test
